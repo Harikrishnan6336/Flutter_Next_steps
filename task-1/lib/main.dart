dd8d9e2ac296e988827d73facd6fe96fae96ca07
Hari Krishnan U <harikrishnan6336@cet.ac.in>
2020-03-22 16:07:04 +0530

    Add files via upload

---
+++ task-1/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: MyHomePage(title: 'Tinkerhub Task 1'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+void _decrementCounter(){
+    setState(() {
+      _counter--;
+    });
+}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display2,
+            ),
+            FlatButton(
+              color: Colors.green,
+              onPressed: _incrementCounter,
+              child: Text('Add'),
+            ),
+            FlatButton(
+              color: Colors.red,
+              onPressed: _decrementCounter,
+              child: Text('Remove'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
