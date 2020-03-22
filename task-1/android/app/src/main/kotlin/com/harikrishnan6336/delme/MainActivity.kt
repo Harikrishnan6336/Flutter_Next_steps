52dba0350a47e7db35b6edc26d4f9a514723ae8b
Hari Krishnan U <harikrishnan6336@cet.ac.in>
2020-03-22 16:02:28 +0530

    Add files via upload

---
+++ task-1/android/app/src/main/kotlin/com/harikrishnan6336/delme/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.harikrishnan6336.delme
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
